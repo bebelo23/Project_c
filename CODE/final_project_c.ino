b1f21db339720399f5c1eb076b208d42c4fa249c
noonecanknowme <118843029+bebelo23@users.noreply.github.com>
2023-10-27 18:26:56 +0700

    plant watering system w/comment

--- CODE/final_project_c.ino
+++ CODE/final_project_c.ino
@@ -1,12 +1,13 @@
-#define BLYNK_TEMPLATE_ID "TMPL6ny8ENg4V"
-#define BLYNK_TEMPLATE_NAME "Plant Watering System"
-#define BLYNK_AUTH_TOKEN "dAfTP9_CCFU3yQtux7hi6u9MuOVaYZDe"
+#define BLYNK_TEMPLATE_ID "TMPL6ny8ENg4V"  // กำหนด ID ของ Blynk Template
+#define BLYNK_TEMPLATE_NAME "Plant Watering System"  // กำหนดชื่อของ Blynk Template
+#define BLYNK_AUTH_TOKEN "dAfTP9_CCFU3yQtux7hi6u9MuOVaYZDe"  // กำหนด Blynk Auth Token
+
 #include <Wire.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
-#define sensor 33
-#define relay 4
+#define sensor 33  // กำหนดขาที่ต่อกับเซ็นเซอร์อัลตราโซนิก (ultrasonic sensor)
+#define relay 4  // กำหนดขาที่ต่อกับรีเลย์
 
 BlynkTimer timer;
 
@@ -20,37 +21,36 @@ char pass[] = "00000000";
 void setup() {
   // Debug console
   Serial.begin(115200);
-  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);  // เริ่มต้นการเชื่อมต่อ Blynk ด้วย Auth Token, SSID และ Password
 
   pinMode(relay, OUTPUT);
-  digitalWrite(relay, HIGH);
+  digitalWrite(relay, HIGH);  // ตั้งค่าขารีเลย์ให้ปิดเริ่มต้น
 }
 
-//Get the ultrasonic sensor values
+// ฟังก์ชันสำหรับดึงค่าเซ็นเซอร์อัลตราโซนิก (ultrasonic sensor)
 void soilMoisture() {
-  int value = analogRead(sensor);
-  value = map(value, 0, 4095, 0, 100);
+  int value = analogRead(sensor);  // อ่านค่าจากเซ็นเซอร์
+  value = map(value, 0, 4095, 0, 100);  // แปลงค่าให้อยู่ในช่วง 0-100
 
-  Blynk.virtualWrite(V0, value);
-  Serial.println(value);
-  if (value <= 50)
-  {
-    Blynk.logEvent("plant_watering_system");
+  Blynk.virtualWrite(V0, value);  // ส่งค่าไปยังแอป Blynk ที่ Widget V0
+  Serial.println(value);  // แสดงค่าที่ Serial Monitor
+  if (value <= 50) {
+    Blynk.logEvent("plant_watering_system");  // ถ้าค่าน้ำเซ็นเซอร์ต่ำกว่าหรือเท่ากับ 50 ให้บันทึกเหตุการณ์ "plant_watering_system" ใน Blynk
   }
 }
 
-//Get the button value
+// ฟังก์ชันสำหรับตรวจสอบค่าจากปุ่มที่แอป Blynk กด (Widget Button)
 BLYNK_WRITE(V1) {
-  bool Relay = param.asInt();
+  bool Relay = param.asInt();  // อ่านค่าที่ส่งมาจาก Widget V1
   if (Relay == 1) {
-    digitalWrite(relay, LOW);
+    digitalWrite(relay, LOW);  // เปิดรีเลย์
   } else {
-    digitalWrite(relay, HIGH);
+    digitalWrite(relay, HIGH);  // ปิดรีเลย์
   }
 }
 
 void loop() {
-  soilMoisture();
-  Blynk.run();  // Run the Blynk library
-  delay(200);
+  soilMoisture();  // เรียกใช้ฟังก์ชัน soilMoisture() เพื่อดึงค่าเซ็นเซอร์
+  Blynk.run();  // ให้ Blynk library ทำงาน
+  delay(200);  // หน่วงเวลาเล็กน้อยเพื่อป้องกันการทำงานเกินไป
 }
